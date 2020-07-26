af13a93b9cb48b5f06fe4cfca1ea90955a4f807e
Mike Yoon <32911840+MikeHYoon@users.noreply.github.com>
2020-07-26 02:14:08 -0700

    Add files via upload
    Transfer money from one account to another function added.
    A 3rd bank account "Transfer" added.
    Transfer accounts can only be transferred to and form, or view the existing balance (Cannot deposit or withdraw).

--- BankingApplicationWithClasses/src/com/practice/Bank.java
+++ BankingApplicationWithClasses/src/com/practice/Bank.java
@@ -4,14 +4,14 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Bank {
-    private static double[] balance = {0.0, 0.0};
+    private static double[] balance = {0.0, 0.0, 0.0};
 
     public static int selectBankAccount() {
         //Asks the user to select which account they would like to access
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please select a Bank account: Checking (1), Savings (2), Terminate (0): ");
+        System.out.print("Please select a Bank account: Checking (1), Savings (2), Transfer (3), Terminate (0): ");
         int option = scanner.nextInt();
-        if(option<0 || option>2) //Returns an error message if the input is invalid
+        if(option<0 || option>3) //Returns an error message if the input is invalid
             System.out.println("Invalid input, please try again.");
         return option; //returns the user's input
     }
@@ -19,9 +19,10 @@ public class Bank {
     public static int operationInput() {
         //Asks the user to select which operation they would like to perform
         Scanner scanner4 = new Scanner(System.in);
-        System.out.print("Please select an operation: Deposit (1), Withdraw (2), Check Checking Balance (3), Terminate (0): ");
+        System.out.print("Please select an operation: Deposit (1), Withdraw (2), Check Balance (3), Transfer Money (4), Terminate (0): ");
+        System.out.println("NOTE: You can only transfer money and check the balance for a transfer account!");
         int option2 = scanner4.nextInt();
-        if(option2<0 || option2>3) //Returns an error message if the input is invalid
+        if(option2<0 || option2>4) //Returns an error message if the input is invalid
             System.out.println("Invalid input, please try again.");
         return option2; //returns the user's input
     }
@@ -29,32 +30,84 @@ public class Bank {
     public static void Deposit(int input){
         //Asks the user how much they would like to deposit
         Scanner scanner2 = new Scanner(System.in);
-        System.out.print("Please enter the amount you would like to Deposit: (Example format: 16.32 = $16.32): ");
+        System.out.print("Please enter the amount you would like to Deposit (Example format: 16.32 = $16.32): ");
         double deposit = scanner2.nextDouble();
         if (deposit < 1.00) { //Returns an error message if the input is less than $1
-            System.out.println("You must deposit at least $1, please try again");
+            System.out.println("You must deposit at least $1, please try again.");
         } else {
             balance[input] = balance[input] + deposit;
-            System.out.println("Deposit Successful");
+            System.out.println("Deposit Successful.");
         }
     }
 
     public static void Withdraw(int input){
         //Asks the user how much they would like to withdraw
         Scanner scanner3 = new Scanner(System.in);
-        System.out.print("Please enter the amount you like to Withdraw: (Example format: 17.55 = $17.55): ");
+        System.out.print("Please enter the amount you like to Withdraw (Example format: 17.55 = $17.55): ");
         double withdraw = scanner3.nextDouble();
         if (withdraw > balance[input]) { //Returns an error message if the withdraw request is greater than the balance
             System.out.println("You do not have enough balance to withdraw this amount, please try again.");
         } else {
             balance[input] = balance[input] - withdraw;
-            System.out.println("Withdraw Successful");
+            System.out.println("Withdraw Successful.");
         }
     }
 
     public static void getBalance(int input){
         //prints the balance of the selected bank account
         String currency = NumberFormat.getCurrencyInstance().format(balance[input]);
-        System.out.println("Your current Checking balance is " + currency);
+        System.out.println("Your current balance is " + currency + ".");
+    }
+
+    public static void transferMoney(int input){
+        //function that transfers money from one account to another
+        //first checks if the account the user wants to transfer from has no balance
+        if(balance[input] == 0.0){
+            System.out.println("You cannot perform a transfer with a $0 balance, please deposit some money first.");
+        }
+        else {
+            Scanner scanner5 = new Scanner(System.in);
+            System.out.print("Please select the account you would like to transfer to (1 for Checkings, 2 for Savings, 3 for Transfer): ");
+            int input2 = scanner5.nextInt();
+            if (input2-1 == input) //checks if the account they want to transfer to is the same account they are accessing
+                System.out.println("You cannot transfer money to the same account, please try again.");
+            else if (input2 == 1) {
+                Scanner scanner6 = new Scanner(System.in);
+                System.out.print("Please enter the amount you would like to transfer to your Checkings account (Example format: 17.55 = $17.55): ");
+                double input3 = scanner6.nextDouble();
+                if(input3 > balance[input]){ //checks if the requested transfer amount is greater than the balance
+                    System.out.println("You do not have enough to transfer this amount, please try again.");
+                }
+                else { //amount is removed the accessed account and increased in the requested account
+                    balance[input] = balance[input] - input3;
+                    balance[input2-1] = balance[input2-1] + input3;
+                    System.out.println("Transfer Successful.");
+                }
+            } else if (input2 == 2) {
+                Scanner scanner6 = new Scanner(System.in);
+                System.out.print("Please enter the amount you would like to transfer to your Savings account (Example format: 17.55 = $17.55): ");
+                double input3 = scanner6.nextDouble();
+                if(input3 > balance[input]){
+                    System.out.println("You do not have enough to transfer this amount, please try again.");
+                }
+                else {
+                    balance[input] = balance[input] - input3;
+                    balance[input2-1] = balance[input2-1] + input3;
+                    System.out.println("Transfer Successful.");
+                }
+            } else if (input2 == 3) {
+                Scanner scanner6 = new Scanner(System.in);
+                System.out.print("Please enter the amount you would like to transfer to your Transfer account (Example format: 17.55 = $17.55): ");
+                double input3 = scanner6.nextDouble();
+                if(input3 > balance[input]){
+                    System.out.println("You do not have enough to transfer this amount, please try again.");
+                }
+                else {
+                    balance[input] = balance[input] - input3;
+                    balance[input2-1] = balance[input2-1] + input3;
+                    System.out.println("Transfer Successful.");
+                }
+            }
+        }
     }
 }
--- BankingApplicationWithClasses/src/com/practice/Main.java
+++ BankingApplicationWithClasses/src/com/practice/Main.java
@@ -11,32 +11,51 @@ public class Main extends Bank {
             //Asks the user to select either their Checking or Savings account
             option = selectBankAccount();
             //If Checking is selected, checks the following options afterwards
-            //1 = deposit, 2 = withdraw, 3 = check balance
+            //1 = deposit, 2 = withdraw, 3 = check balance, 4 = transfer money
             if (option == 1) {
                 option2 = operationInput();
                 if (option2 == 1) {
-                    Deposit(0);
+                    bank.Deposit(0);
                 }
                 if (option2 == 2) {
-                    Withdraw(0);
+                    bank.Withdraw(0);
                 }
                 if (option2 == 3) {
-                    getBalance(0);
+                    bank.getBalance(0);
                 }
-
+                if (option2 == 4) {
+                    bank.transferMoney(0);
+                }
             }
             //If Savings is selected, checks the following options afterwards
-            //1 = deposit, 2 = withdraw, 3 = check balance
+            //1 = deposit, 2 = withdraw, 3 = check balance, 4 = transfer money
             if (option == 2) {
                 option2 = operationInput();
                 if (option2 == 1) {
-                    Deposit(1);
+                    bank.Deposit(1);
                 }
                 if (option2 == 2) {
-                    Withdraw(1);
+                    bank.Withdraw(1);
                 }
                 if (option2 == 3) {
-                    getBalance(1);
+                    bank.getBalance(1);
+                }
+                if (option2 == 4) {
+                    bank.transferMoney(1);
+                }
+            }
+            //If Transfer is selected, checks the following options afterwards
+            //3 = check balance, 4 = transfer money
+            if (option == 3) {
+                option2 = operationInput();
+                if (option2 == 1 || option2 == 2) {
+                    System.out.println("Cannot perform a deposit or withdraw with a transfer account, please try again.");
+                }
+                else if (option2 == 3) {
+                    bank.getBalance(2);
+                }
+                else if (option2 == 4) {
+                    bank.transferMoney(2);
                 }
             }
         }
